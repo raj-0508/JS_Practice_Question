7b4b2d0dc80a11f3f1e89b701acbcc1f1164ea39
Raj Singhaniya <rajsinghaniya0508@gmail.com>
2025-02-04 19:08:56 +0530

    js_practice Question _1

---
+++ Prctice_Question_01.js
@@ -0,0 +1,90 @@
+// 1. Write a program which enter the age of a person and if the age is 18 or more than 18 then print you are adult, otherwise print you are minor.
+
+// let age = 33
+// if (age >= 18){
+//     console.log("you are adult");
+// }else{
+//     console.log("you are minor");
+// }
+
+// By conditional (ternary) operator
+// let age = 25
+// console.log(age >=18 ? "you are adult" : "you rae minor");
+
+
+
+
+// 2. Write a program, which enters marks in three subject of a students and calculate its average. if the average is 33 or more than print pass, otherwise print fail.
+
+// let sub1 = 68
+// let sub2 = 20
+// let sub3 = 42
+// avg = ((sub1 + sub2 + sub3) / 3)
+// if (avg >= 33){
+//     console.log("you are pass");
+// }else{
+//     console.log("you are fail");
+// }
+
+// By conditional (ternary) operator
+// let sub1 = 68
+// let sub2 = 20
+// let sub3 = 42
+// console.log(avg >= 33 ? "you are pass" : "you are fail");
+
+
+
+
+// 3. Write a program which enter any integer then check and print, whether it is odd no. or even no.
+
+// let num = 35
+// if (num % 2 == 0){
+//     console.log("it is even no");
+// }else{
+//     console.log("it is odd no");
+// }
+
+// By conditional (ternary) operator
+// let num = 35
+// console.log(num % 2 == 0 ? "it is even no" : "it is odd no");
+
+
+
+// 4. Write a program which enter any integer and if it is less than 5 than calculation and print its cube otherwise calculate and prints its square.
+
+// let num = 4
+// if (num < 5){
+//     console.log(num ** 3);
+// }else{
+//     console.log(num ** 2);
+// }
+
+// By conditional (ternary) operator
+// let num = 4
+// console.log(num < 5 ? num ** 3 : num ** 2);
+
+
+
+// 5. Write a MENU DRIVEN program which enter two integer and perform any one of the giveen arthemethical calculatin, depending one of the user choice from menu.
+	// 1. ADDITION
+	// 2. SUBSTRACTION
+	// 3. MULTIPLICATION
+	// 4. DIVISION
+
+// let num1 = 95
+// let num2 = 45
+
+// switch (4) {
+//     case 1:
+//         console.log(num1 + num2);
+//         break;
+//     case 2:
+//         console.log(num1 - num2);
+//         break;
+//     case 3:
+//         console.log(num1 * num2);
+//         break;
+//     case 4:
+//         console.log(num1 / num2);
+//         break;
+// }
