4ef21622110298e66511d085f4b20c0f6c9e1a63
Raj Singhaniya <rajsinghaniya0508@gmail.com>
2025-02-06 10:48:42 +0530

    Loop Practice Questions

---
+++ Loops_Questions.js
@@ -0,0 +1,39 @@
+// Q1. Write a program which generate and print number from 5 to 10.
+
+// for (let i = 5; i <=10; i++) {
+//     console.log(i);
+// }
+
+
+
+
+
+// Q2. Write a program which define any integer and which also enters number of times the enter number to be printed.
+
+// let num = 55
+// let times = 5
+// for (let i = 0; i < times; i++) {
+//     console.log(num);
+// }
+
+
+
+
+
+
+// Q3. Write a program which enter any integer and print its multiplication table in table format
+// format- 
+//   8 * 1 = 8
+
+
+// num = 49
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${num} * ${i} = ${num * i}`);
+// }
+
+
+
+
+
+// Q4. Write a program which enter any integer and print in reverse order.
+
